1797ffa83b9cf47f7240789975eea467f0d0a184
“Aosarise” <984738104@qq.com>
2021-03-21 14:05:34 +0800

    base init

---
+++ Base/src/main/java/pojo/Paper.java
@@ -0,0 +1,67 @@
+package pojo;
+
+public class Paper {
+    int id;
+    String title;
+    String abstractContent;
+    String year;
+    String authors;
+    String meeyting;
+    String link;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAbstractContent() {
+        return abstractContent;
+    }
+
+    public void setAbstractContent(String abstractContent) {
+        this.abstractContent = abstractContent;
+    }
+
+    public String getYear() {
+        return year;
+    }
+
+    public void setYear(String year) {
+        this.year = year;
+    }
+
+    public String getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(String authors) {
+        this.authors = authors;
+    }
+
+    public String getMeeyting() {
+        return meeyting;
+    }
+
+    public void setMeeyting(String meeyting) {
+        this.meeyting = meeyting;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+}
