eee65cbaef31befc421dcba439627088c97ff9c5
czj6 <673330108@qq.com>
2021-03-21 20:04:04 +0800

    完成论文列表静态页面

--- 081800306&041802224/app.js
+++ 081800306&041802224/app.js
@@ -1,16 +1,17 @@
-const koa = require('koa')
-const app = new koa()
+const Koa = require('koa');
+
+const app = new Koa();
 app.use(async (ctx, next) => {
   if (ctx.request.path === '/') { // 首页
-    ctx.response.status = 200
-    ctx.response.body = 'index'
-  } else if (ctx.request.path === '/content') { 
-    ctx.response.status = 200
-    ctx.response.body = 'list'
+    ctx.response.status = 200;
+    ctx.response.body = 'index';
+  } else if (ctx.request.path === '/content') {
+    ctx.response.status = 200;
+    ctx.response.body = 'list';
   } else {
-    ctx.throw(404, 'Not found') // 404
+    ctx.throw(404, 'Not found'); // 404
   }
-await next()
-})
+  await next();
+});
 
-app.listen(3000)
+app.listen(3000);
