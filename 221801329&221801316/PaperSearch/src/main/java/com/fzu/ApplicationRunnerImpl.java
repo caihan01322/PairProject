f54b9cd5d641d505fd1035d74ee46ef0dd75917e
Liangdayi <1113901523@qq.com>
2021-03-22 22:26:37 +0800

    完善三个会议的json文件解析
    由于三个会议的json文件格式有不同之处，故分成多个方法针对不同情况进行解析，其中需要注意一些内容为空时的异常判断

--- 221801329&221801316/PaperSearch/src/main/java/com/fzu/ApplicationRunnerImpl.java
+++ 221801329&221801316/PaperSearch/src/main/java/com/fzu/ApplicationRunnerImpl.java
@@ -29,9 +29,12 @@ public class ApplicationRunnerImpl implements ApplicationRunner {
                 JSONObject jsonObject=JSONObject.parseObject(json);
                 String title=jsonObject.getString("title");
                 String abstractContent=jsonObject.getString("abstract");
+                if(abstractContent.length()>=150){
+                    abstractContent=abstractContent.substring(0,150);
+                }
                 String link=jsonObject.getString("doiLink");
                 String meet="CVPR";
-                Integer year=jsonObject.getInteger("publicationYear");
+                Integer year=Integer.valueOf(jsonObject.getString("publicationYear"));
                 List<String> keywordList=new ArrayList<>();
                 JSONArray keywords= jsonObject.getJSONArray("keywords");
                 if(keywords!=null){
@@ -59,7 +62,7 @@ public class ApplicationRunnerImpl implements ApplicationRunner {
                     authorList.add(" ");
                 }
                 paper.setTitle(title);
-                paper.setAbstractContent(abstractContent.substring(0,150));
+                paper.setAbstractContent(abstractContent);
                 paper.setAuthor(authorList);
                 paper.setKeywords(keywordList);
                 paper.setLink(link);
@@ -70,6 +73,115 @@ public class ApplicationRunnerImpl implements ApplicationRunner {
         }
         else{
             System.out.println("文件不存在");
+        }
+        return paperList;
+    }
+    public List<Paper> iccvJsonParse(){
+        List<Paper> paperList=new ArrayList<>();
+        String dir=System.getProperty("user.dir");
+        System.out.println(dir);
+        File file=new File(dir+"/src/main/resources/论文数据/3");
+        if(file.exists()){
+            File []child=file.listFiles();
+            for(int i=0;i<child.length;i++){
+                Paper paper=new Paper();
+                String json=jsonRead(child[i]);
+                json=json.replace(";","");
+                JSONObject jsonObject=JSONObject.parseObject(json);
+                String title=jsonObject.getString("title");
+                String abstractContent=jsonObject.getString("abstract");
+                if(abstractContent.length()>=150){
+                    abstractContent=abstractContent.substring(0,150);
+                }
+                String link=jsonObject.getString("doiLink");
+                String meet="ICCV";
+                Integer year=Integer.valueOf(jsonObject.getString("publicationYear"));
+                List<String> keywordList=new ArrayList<>();
+                JSONArray keywords= jsonObject.getJSONArray("keywords");
+                if(keywords!=null){
+                    for(int j=0;j<keywords.size();j++){
+                        JSONObject keyword=keywords.getJSONObject(j);
+                        JSONArray jsonArray=keyword.getJSONArray("kwd");
+                        for(int k=0;k<jsonArray.size();k++){
+                            System.out.println(i+"      "+jsonArray.getString(k));
+                            keywordList.add(jsonArray.getString(k));
+                        }
+                    }
+                }
+                else {
+                    keywordList.add(" ");
+                }
+                List<String> authorList=new ArrayList<>();
+                JSONArray authors=jsonObject.getJSONArray("authors");
+                if(authors!=null){
+                    for(int j=0;j<authorList.size();j++){
+                        JSONObject author=authors.getJSONObject(j);
+                        authorList.add(author.getString("name"));
+                    }
+                }
+                else{
+                    authorList.add(" ");
+                }
+                paper.setTitle(title);
+                paper.setAbstractContent(abstractContent);
+                paper.setAuthor(authorList);
+                paper.setKeywords(keywordList);
+                paper.setLink(link);
+                paper.setMeet(meet);
+                paper.setYear(year);
+                paperList.add(paper);
+            }
+        }
+        else{
+            System.out.println("文件不存在");
+        }
+        return paperList;
+    }
+    public List<Paper> eccvJsonParse(){
+        List<Paper> paperList=new ArrayList<>();
+        String dir=System.getProperty("user.dir");
+        System.out.println(dir);
+        File file=new File(dir+"/src/main/resources/论文数据/2");
+        if(file.exists()){
+            File []child=file.listFiles();
+            for(int i=0;i<child.length;i++){
+                Paper paper=new Paper();
+                String json=jsonRead(child[i]);
+                json=json.replace(";","");
+                JSONObject jsonObject=JSONObject.parseObject(json);
+                String title=jsonObject.getString("论文名称");
+                String abstractContent=jsonObject.getString("摘要");
+                if(abstractContent.length()>=150){
+                    abstractContent=abstractContent.substring(0,150);
+                }
+
+                System.out.println(i+" "+abstractContent);
+                String link=jsonObject.getString("原文链接");
+                String author="未知";
+                String meet="ECCV";
+                List<String> keywordList=new ArrayList<>();
+                JSONArray jsonArray=jsonObject.getJSONArray("关键词");
+                if(jsonArray!=null){
+                    for(int j=0;j<jsonArray.size();j++){
+                        keywordList.add(jsonArray.getString(j));
+                    }
+                }
+                else{
+                    keywordList.add(" ");
+                }
+                List<String> authorList=new ArrayList<>();
+                authorList.add(author);
+                String year=jsonObject.getString("发布时间");
+                year=year.substring(year.length()-4,year.length());
+                paper.setTitle(title);
+                paper.setMeet(meet);
+                paper.setLink(link);
+                paper.setYear(Integer.valueOf(year));
+                paper.setAbstractContent(abstractContent);
+                paper.setAuthor(authorList);
+                paper.setKeywords(keywordList);
+                paperList.add(paper);
+            }
         }
         return paperList;
     }
@@ -92,6 +204,6 @@ public class ApplicationRunnerImpl implements ApplicationRunner {
     }
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        List<Paper> paperList=cvprJsonParse();
+        List<Paper> paperList=eccvJsonParse();
     }
 }
