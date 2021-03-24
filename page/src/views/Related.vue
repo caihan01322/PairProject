5049771e83754a7fc1f5324c16b1acd91f3d3ba1
Ruleer <41296448+Ruleer@users.noreply.github.com>
2021-03-24 09:44:47 +0800

    new file:   .gitignore 	new file:   page/README.md 	new file:   page/babel.config.js 	new file:   page/package-lock.json 	new file:   page/package.json 	new file:   page/public/favicon.ico 	new file:   page/public/index.html 	new file:   page/src/App.vue 	new file:   page/src/assets/logo.png 	new file:   page/src/main.js 	new file:   page/src/router/index.js 	new file:   page/src/store/index.js 	new file:   page/src/views/Analyze.vue 	new file:   page/src/views/Detail.vue 	new file:   page/src/views/Edit.vue 	new file:   page/src/views/List.vue 	new file:   page/src/views/Related.vue

---
+++ page/src/views/Related.vue
@@ -0,0 +1,23 @@
+<!--  -->
+<template>
+    <div class='related'></div>
+</template>
+
+<script>
+
+export default {
+    name: 'Related',
+    components: {},
+    data () {
+        return {
+
+        }
+    },
+    methods: {
+
+    }
+}
+</script>
+<style lang='scss' scoped>
+//@import url();
+</style>
