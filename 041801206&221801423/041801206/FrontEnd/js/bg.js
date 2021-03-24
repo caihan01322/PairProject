31071349e517d89eaf31b117071fa61cd5ba1162
Pilipala-cmy <70570316+Pilipala-cmy@users.noreply.github.com>
2021-03-24 19:58:00 +0800

    编译原理实践课上做了一些

--- 041801206&221801423/041801206/FrontEnd/js/bg.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    bgImageTotal=20;
-    randomNumber = Math.round(Math.random()*(bgImageTotal-1))+1;
-    imgPath=('/img/bg/'+randomNumber+'.jpg');
-    $('h1').css('background-image', ('url("'+imgPath+'")'));
-});
