3c3261e3381997d45e8fc741f641b4dbb6f9b643
6586744 <67788880+6586744@users.noreply.github.com>
2021-03-24 21:29:48 +0800

    建立分支

---
+++ 221801301&221801303/server/src/main/java/com/fzu/server/pojo/ECCV.java
@@ -0,0 +1,15 @@
+package com.fzu.server.pojo;
+
+import java.util.List;
+
+public class ECCV {
+    int ID;
+    String Abstract;
+    String year;
+    String time;
+    String name;
+    String link;
+    List keyword;
+
+    
+}
