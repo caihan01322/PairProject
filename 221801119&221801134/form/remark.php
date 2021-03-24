654a3b8e5a2c503ad940bee44cd5d7cf89c7fb66
SearlShieh <632806635@qq.com>
2021-03-24 13:17:08 +0800

    实现关键词跳转功能

--- 221801119&221801134/form/remark.php
+++
@@ -1,9 +0,0 @@
-<?php
-$pid=$_POST['pid'];
-$userid=$_POST['userid'];
-$remark=$_POST['remark'];
-$conn = new mysqli('localhost','root','','paperdb');
-$sql = "update userPaper set remarks='$remark'  where pid = '$pid' and userid='$userid' ";
-$conn->query($sql);
-echo '<script>alert("修改成功！");window.location.href="../view/manage.php";</script>';
-?>
