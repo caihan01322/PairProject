17c29e9ad7d12390b3c9786c53c5305552305ada
miss-pud-in <79395007+miss-pud-in@users.noreply.github.com>
2021-03-25 18:53:09 +0800

    论文导入页面添加词云
    1、在搜索框上方添加关键词组成的词云
    2、点击词云可跳转至相关论文

--- 221801119&221801134/view/import.php
+++ 221801119&221801134/view/import.php
@@ -10,6 +10,8 @@
     <link rel='stylesheet' href='../css/myCss2.css' type='text/css'/>
     <link rel='stylesheet' href='../css/myCss4.css' type='text/css'/>
     
+    <script src='https://cdn.bootcss.com/echarts/3.7.0/echarts.simple.js'></script>
+    <script src='../plug-in/echarts-wordcloud.js'></script>
     <script>
         function findFile(){
         	var inputObj=document.createElement('input')
@@ -23,7 +25,7 @@
     </script>
 </head>
 
-<body style="background-color:#F8F8F8 ;">
+<body style="background-color: #F8F8F8;">
 <div id="app" class="surface--content">
     <header class="metabar">
         <div class="container u-flex">
@@ -75,9 +77,133 @@
 </div>
 
 <div id="import_container">
-    <div id="search_img_wrap">
-        <svg t="1616296868325" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="2105" xmlns:xlink="http://www.w3.org/1999/xlink" width="200" height="200"><defs><style type="text/css"></style></defs><path d="M510.81 288a224.45 224.45 0 1 0 158.71 65.74A222.93 222.93 0 0 0 510.81 288z m0 384.9c-88.48 0-160.45-72-160.45-160.45S422.33 352 510.81 352s160.45 72 160.45 160.45S599.28 672.9 510.81 672.9z" p-id="2106" fill="#515151"></path><path d="M893.36 350.83a415.59 415.59 0 1 0-163.74 514.54l-46.81-46.81a349.1 349.1 0 0 1-172 45.08c-193.65 0-351.2-157.54-351.2-351.19s157.55-351.2 351.2-351.2S862 318.8 862 512.45a349 349 0 0 1-43.89 169.87L865 729.21a416 416 0 0 0 28.35-378.38z" p-id="2107" fill="#515151"></path><path d="M690.616 735.875l45.254-45.254 157.204 157.204-45.254 45.254z" p-id="2108" fill="#515151"></path></svg>
-    </div>
+    <div id="search_word_cloud" style="width: 80%; height: 480px; margin: 0 0 0 5%; padding: 0px;"></div>
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "paperdb";
+    // 数据库连接
+    $conn = new mysqli($servername, $username, $password, $dbname);
+    // Check connection
+    if ($conn->connect_error) {
+        die("连接失败: " . $conn->connect_error);
+    }
+    function getTopKeys($sql, $conn, $nums) {
+        $result = $conn->query($sql);
+        $result_array = array();
+        $key_array = array();
+        if ($result->num_rows > 0) {
+            while($row = $result->fetch_assoc()) {
+                $result_array[] = $row["key1"]; 
+                $result_array[] = $row["key2"];
+                $result_array[] = $row["key3"];
+                $result_array[] = $row["key4"];
+                $result_array[] = $row["key5"];
+            }
+        }
+        foreach($result_array as $value){
+            if(!empty($value)){
+                $value = ucfirst($value);
+                //$value = str_replace(' ', '<br>', $value);  //词组遇空格则分行
+                if(array_key_exists($value, $key_array)){
+                    $key_array[$value] += 10;   //+=1
+                }
+                else{
+                    $key_array[$value] = 10;   //=1
+                }
+            }
+        }
+        arsort($key_array);
+        return array_slice($key_array, 0, $nums);
+    }
+    
+    $raw_arr = array();
+    $raw_arr = getTopKeys("SELECT key1, key2, key3, key4, key5 FROM paper", $conn,300);
+    $key_arr = array_keys($raw_arr);
+    $value_arr = array_values($raw_arr);
+    //生成支持词云的data
+    $arr = array();
+    $arr_tmp = array();
+    $size = count($raw_arr);
+    for($i = 0; $i < $size; $i++){
+        $arr_tmp["name"] = $key_arr[$i];
+        $arr_tmp["value"] = $value_arr[$i];
+        $arr[] = $arr_tmp;
+    }
+    
+    echo 
+"<script>
+          var myChart = echarts.init(document.getElementById('search_word_cloud'));
+
+          var maskImage = new Image();
+          maskImage.src = \"../img/logo.png\";
+
+          var option = {
+        		    title: {
+        		        text: '词云',//标题
+        		        x: 'center',
+        		        textStyle: {
+        		            fontSize: 23
+        		        }
+        		 
+        		    },        		    
+        		    tooltip: {
+        		        show: true
+        		    },
+        		    series: [{
+        		        type: 'wordCloud',
+        		        gridSize: 1,
+        		        sizeRange: [6, 66],  //画布范围，如果设置太大会出现少词（溢出屏幕）
+        		        rotationRange: [-45, 90],  //数据翻转范围
+        		        maskImage: maskImage,
+        		        shape: 'circle',
+        		        textPadding: 0,
+        		        autoSize: {
+        		            enable: true,
+        		            minSize: 6
+        		        },
+        		        textStyle: {
+        		            normal: {
+        		               color: function() {
+                                    //var colors = new Array('#FE4365', '#FC9D9A', '#F9CDAD', '#FFAEB9', '#EEA2AD', '#CD8C95', '#8B5F65');
+                                    var colors = new Array('#D3D3D3', '#FFC1C1', '#CDA49E', '#C51F1F', '#9C2632', '#471F1F');
+                                    var index = Math.ceil(Math.random() * (colors.length - 1));
+        		                    return colors[index];
+        		                }
+        		            },
+        		            emphasis: {
+        		                shadowBlur: 10,
+        		                shadowColor: '#333'
+        		            }
+        		        },
+    		            left: 'center',
+                        top: 'center',
+                        right: null,
+                        bottom: null,
+        		        data:".json_encode($arr).", 
+        		    }] //end of series
+        		}; //end of option
+          maskImage.onload = function(){
+              myChart.setOption(option);
+          }
+          myChart.on('click',function(params){
+			  var name = params.name;
+			  //alert(name + \":\" + value);
+              window.location.href = \"search.php?searchName=\" + name + \"&searchSelect=3\";
+		  });
+          myChart.hover(function(params){
+			  var name = params.name;
+			  alert(name + \":\" + value);
+              //window.location.href = \"search.php?searchName=\" + name + \"&searchSelect=3\";
+		  });
+          window.onresize = function () {
+              myChart.resize();
+          }
+    </script>";
+    $conn->close();
+?>
+    
 	<div id="search_container">
 		<form id="search_form" action="#" method="post">
             <svg onclick="findFile()" t="1616297592638" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg" p-id="4952" data-spm-anchor-id="a313x.7781069.0.i4" width="40" height="40"><path d="M938.3936 464.896l-158.72 300.35968c-9.96352 21.0944-30.14656 29.16352-39.40352 29.16352l-612.00384 0.13312c-24.77056 0-44.92288-20.15232-44.92288-44.92288l0.77824-385.65888L83.74272 250.05056c-0.03072-5.45792 2.10944-10.69056 5.94944-14.56128 3.85024-3.87072 9.08288-6.0416 14.53056-6.0416l159.1808 0c8.4992 0 16.10752 5.25312 19.13856 13.18912l29.11232 76.41088 174.17216 0c-17.23392 11.264-33.49504 24.96512-48.85504 40.96L297.54368 360.00768c-8.4992 0-16.10752-5.25312-19.13856-13.18912l-29.11232-76.41088L124.29312 270.40768l0.02048 93.44 0 385.78176c0 2.18112 1.78176 3.96288 3.96288 3.96288l48.11776 0L319.09888 464.896c8.05888-16.14848 23.28576-29.16352 39.40352-29.16352l22.74304 0 170.26048 0 35.26656 0c-23.71584 17.29536-33.9968 48.93696-22.97856 77.6192 9.82016 25.53856 34.78528 42.69056 62.12608 42.69056 8.16128 0 16.1792-1.49504 23.8592-4.43392l179.24096-68.84352c21.3504-8.20224 36.05504-26.28608 40.92928-47.03232l39.28064 0C925.3376 435.74272 947.87584 446.6176 938.3936 464.896zM438.91712 409.6l91.8528 0c0.80896 0 1.50528 0.17408 2.28352 0.22528 17.32608-15.88224 35.77856-27.22816 55.296-33.4848 56.33024-18.05312 112.85504 6.8608 145.12128 26.112l-107.48928 41.27744c-18.4832 7.09632-27.70944 27.83232-20.61312 46.30528 5.4784 14.25408 19.05664 22.99904 33.46432 22.99904 4.28032 0 8.62208-0.768 12.8512-2.38592l179.23072-68.84352c18.4832-7.09632 27.70944-27.83232 20.61312-46.30528L782.68416 216.2688c-7.08608-18.4832-27.8528-27.67872-46.30528-20.61312-18.4832 7.09632-27.6992 27.83232-20.60288 46.30528l33.54624 87.3472c-45.13792-22.81472-111.77984-43.73504-182.272-21.42208C517.77536 323.52256 475.00288 357.55008 438.91712 409.6z" p-id="4953" fill="#686f72" data-spm-anchor-id="a313x.7781069.0.i5" class="selected"></path></svg>			<input type="text" name="condition" placeholder="请输入搜索条件">
@@ -87,11 +213,12 @@
 </div>
 
 </body>
-</html>
 
-<<script type="text/javascript">
+<script type="text/javascript">
 	function exitLogin(){
 	window.event.returnValue=false;     
 	window.location.href="../form/exit.php";
 }
 </script>
+
+</html>
