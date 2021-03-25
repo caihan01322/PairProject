ea67bb70d1766fe0a246002600abb4c978226791
hpc <1132874992@qq.com>
2021-03-25 16:03:11 +0800

    新建编辑论文列表页面（html初步模型）

---
+++ PairProjectDemo/tp5/application/index/controller/Edit.php
@@ -0,0 +1,13 @@
+<?php
+namespace app\index\controller;
+
+use think\Controller;
+
+class Edit extends Controller
+{
+    public function index()
+    {
+        return $this->fetch();
+    }
+    
+}
