b740cddeff9d37760fb93e628612ea8963f80740
hpc <1132874992@qq.com>
2021-03-25 22:03:47 +0800

    获取数据库数据，将数据导入列表中

--- PairProjectDemo/tp5/application/index/controller/Edit.php
+++ PairProjectDemo/tp5/application/index/controller/Edit.php
@@ -2,11 +2,17 @@
 namespace app\index\controller;
 
 use think\Controller;
+use think\Db;
 
 class Edit extends Controller
 {
+    
     public function index()
     {
+        $page = 0;
+        $limit = 4;
+        $data = Db::table('paper')->order('title')->limit($page*$limit,$page*$limit+$limit)->select();
+        $this->assign('tableData',json_encode($data));
         return $this->fetch();
     }
     
