63421f33d9f48dc887cc2b17cf6a35a9719b88c7
hpc <1132874992@qq.com>
2021-03-26 16:17:29 +0800

    实现分页器刷新页面

--- PairProjectDemo/tp5/application/index/controller/Edit.php
+++ PairProjectDemo/tp5/application/index/controller/Edit.php
@@ -3,16 +3,17 @@ namespace app\index\controller;
 
 use think\Controller;
 use think\Db;
+use think\Request;
 
 class Edit extends Controller
 {
-    
+    protected $request;
     public function index()
     {
-        $page = 0;
-        $limit = 4;
-        $data = Db::table('paper')->order('title')->limit($page*$limit,$page*$limit+$limit)->select();
+        
         $count = Db::table('paper')->count();
+        $data = Db::table('paper')->select();
+        
         $this->assign('tableData',json_encode($data));
         $this->assign('count',$count);
         return $this->fetch();
