8a87c1cac62087a4f1f97b8ac143bd1c6da5a8ed
tkhhhh <45356508+tkhhhh@users.noreply.github.com>
2021-03-26 16:40:02 +0800

    Add files via upload
    define of function in datadeal

--- dev/backend/deal/Datadeal.py
+++ dev/backend/deal/Datadeal.py
@@ -1,1 +1,24 @@
+import pymysql
 
+
+class DataBase(object):
+    def __init__(self,):
+        self.db = pymysql.connect("localhost",)
+
+    def Search(self,title,keywords,page,status):
+        return
+
+    def TitleTip(self,keyword):
+        return
+    def KeywordTip(self,keyword):
+        return
+    def DeleteArticle(self,title):
+        return
+    def SearchHot(self,keyword,stime,etime):
+        return
+    def CaculateHot(self,meeting,page):
+        return
+    def ReHot(self):
+        return
+    def DeleteTask(self,task):
+        return
