584864efd339f336062bb99432cdc99fbe8e60a3
LiarX <58362731+LiarX@users.noreply.github.com>
2021-03-27 00:51:47 +0800

    第六次commit
    发现public/static文件夹下所有css,js,img都没有上传，修改了static目录下的.gitignore文件并补充上传

---
+++ 221801409&221801418/public/static/index/js/login_interface.js
@@ -0,0 +1,4 @@
+document.getElementById("register").onclick=function(){goToRegistation;}
+function goToRegistation(){
+    window.location="/application/index/controller/RegistrationInterface.php"
+}
