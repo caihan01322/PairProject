2a29f3f6f1c4c0f417db71086f622e07a8c2c83f
LiarX <58362731+LiarX@users.noreply.github.com>
2021-03-27 00:41:44 +0800

    第五次commit
    完成注册界面，实现从登录界面向注册界面的跳转

--- 221801409&221801418/application/index/controller/RegistrationInterface.php
+++ 221801409&221801418/application/index/controller/RegistrationInterface.php
@@ -3,11 +3,11 @@ namespace app\index\controller;
 
 use app\index\model\paper;
 use think\Controller;
-class LoginInterface extends Controller
+class RegistrationInterface extends Controller
 {
 // 创建用户数据页面
     public function create()
     {
-        return view('LoginInterface');//加载模板，会自动找到view下的user的user.htmurl_convertl
+        return view('RegistrationInterface');
     }
 }
