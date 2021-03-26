1c213d386843cc2a23f69923974b9c40d5684b5a
2018FZUSELYK <752792124@qq.com>
2021-03-27 00:36:50 +0800

    commit 10 更新接口

--- 221801329&221801316/view/js/paperList.js
+++ 221801329&221801316/view/js/paperList.js
@@ -1,37 +1,40 @@
-var globalBaseURL = 'http://121.5.100.116:8080/api';
+var globalBaseURL = 'http://120.77.84.235:8080';
 var instance = axios.create({
-    baseURL: 'http://121.5.100.116:8080/api',
+    baseURL: 'http://120.77.84.235:8080',
     headers: {
         'X-Requested-With': 'XMLHttpRequest'
     },
 })
-var searchStmt;
+var searchStmt = '';
 
 function getPaperList(pageNum) {
     searchStmt = document.getElementById("search-text").value;
-    instance.get('/search', {
+    instance.get('/queryByAuthor', {
             params: {
-                keyword: searchStmt,
-                pageNum: pageNum
+                start: pageNum,
+                rows: 10,
+                author: searchStmt
             }
         })
-        .then(function (response) {
+        .then(function(response) {
             panel = document.getElementById('main-panel');
             panel.innerHTML = "";
             //console.log(response.data);
-            var data = response.data['data'];
-            if (data.totalNum === 0) {
+            var data = response.data;
+            // console.log(JSON.stringify(data))
+
+            if (data.length === 0) {
                 panel.innerHTML = panel.innerHTML + "<p style=\"text-align:center;color: rgb(127, 127, 127);\">No result</p>";
             } else {
-                var list = data.list;
+                var list = data;
                 console.log(list);
                 for (var k in list) {
                     var element = list[k];
-                    var abstractStr = element['artical']['Abstract'].slice(0, 100) + "...";
+                    var abstractStr = element['abstractContent'].slice(0, 100) + "...";
                     var authorStr = "";
                     var keywordStr = "";
-                    for (var t in element.authors) {
-                        authorStr += element.authors[t] + ';';
+                    for (var t in element.author) {
+                        authorStr += element.author[t] + ';';
                         if (t >= 3) {
                             break;
                         }
@@ -46,11 +49,11 @@ function getPaperList(pageNum) {
                     keywordStr = keywordStr.slice(0, -1);
                     panel.innerHTML = panel.innerHTML +
                         "<div class=\"paper-list\" id=" +
-                        element.artical.academicNum +
+                        element.id +
                         "><a href=" +
-                        element.artical.link +
+                        element.link +
                         " class=\"paper-title\">" +
-                        element.artical.title +
+                        element.title +
                         "</a>" +
                         "<p class=\"paper-author\">" +
                         authorStr +
@@ -66,7 +69,7 @@ function getPaperList(pageNum) {
                 initPagination(pageNum, list['totalPage']);
             }
         })
-        .then(function (error) {
+        .then(function(error) {
             console.log(error);
         })
 
@@ -123,10 +126,10 @@ function getTopKwords() {
                 pageNum: 'pageNum'
             }
         })
-        .then(function (response) {
+        .then(function(response) {
 
         })
-        .then(function (response) {
+        .then(function(response) {
             console.log(error);
         })
 }
