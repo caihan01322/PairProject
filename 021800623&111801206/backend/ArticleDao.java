d2941fcf848d063c9a35f0d06d6cc3e4edebf723
QAQWMZ <79703668+QAQWMZ@users.noreply.github.com>
2021-03-26 14:58:14 +0800

    增加补充查询条件的查询方法selectSpecial()

--- 021800623&111801206/backend/ArticleDao.java
+++ 021800623&111801206/backend/ArticleDao.java
@@ -49,4 +49,82 @@ public class ArticleDao {
 		
 		return list;
 	}
+	public static ArrayList<Paper> selectSpecial(String conditionString) {
+		
+		ArrayList<Paper> list = new ArrayList<Paper>();
+		
+		ArrayList<Integer> numArrayList = new ArrayList<Integer>(); //判断是否重复包含论文编号
+		
+		ResultSet rs = null;
+		
+		Connection connection = Base.getConn();
+		
+		PreparedStatement ps = null;
+		String sqlString =  "select * from academics where (title like '%"+conditionString+"%')";
+		
+		try {
+			ps = connection.prepareStatement(sqlString);
+			rs = ps.executeQuery();
+			while(rs.next()) {
+				
+				if (!numArrayList.contains(rs.getInt("academicNum"))) {
+					numArrayList.add(rs.getInt("academicNum"));
+					Paper paper = new Paper();
+					paper.setAcademicNum(rs.getInt("academicNum"));
+					paper.setAbstractString(rs.getString("abstract"));
+					paper.setLink(rs.getString("link"));
+					paper.setYear(rs.getString("year"));
+					paper.setTitle(rs.getString("title"));
+					
+					ArrayList<String> keywordStrings = new ArrayList<String>();
+					sqlString = "select * from keywords where academicNum= '"+rs.getInt("academicNum")+"'";
+					PreparedStatement ps1 = connection.prepareStatement(sqlString);
+					ResultSet rs1 = ps1.executeQuery();
+					while(rs1.next()) {
+						keywordStrings.add(rs1.getString("keyword"));
+					}
+					paper.setKeywords(keywordStrings);
+					list.add(paper);
+				}
+			}
+			
+			sqlString =  "select * from keywords where (keyword like '%"+conditionString+"%')";
+			PreparedStatement ps1 = connection.prepareStatement(sqlString);
+			ResultSet rs1 = ps1.executeQuery();
+			while(rs1.next())
+			{
+				if (!numArrayList.contains(rs1.getInt("academicNum"))) {
+					numArrayList.add(rs1.getInt("academicNum"));
+					Paper paper = new Paper();
+					sqlString = "select * from academics where academicNum='"+rs1.getInt("academicNum")+"'";
+					PreparedStatement ps2 = connection.prepareStatement(sqlString);
+					ResultSet rs2 = ps2.executeQuery();
+					rs2.next();
+					paper.setAcademicNum(rs2.getInt("academicNum"));
+					paper.setAbstractString(rs2.getString("abstract"));
+					paper.setLink(rs2.getString("link"));
+					paper.setYear(rs2.getString("year"));
+					paper.setTitle(rs2.getString("title"));
+					ArrayList<String> keywordStrings = new ArrayList<String>();
+					sqlString = "select * from keywords where academicNum= '"+rs2.getInt("academicNum")+"'";
+					PreparedStatement ps3 = connection.prepareStatement(sqlString);
+					ResultSet rs3 = ps3.executeQuery();
+					while(rs3.next()) {
+						keywordStrings.add(rs3.getString("keyword"));
+					}
+					paper.setKeywords(keywordStrings);
+					list.add(paper);
+				}
+			}
+			
+			
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return list;
+	}
+}
+
 	
