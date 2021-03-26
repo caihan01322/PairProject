e395953e308859ce03d7749e89b1a4034b6d09c4
微风的火原 <1075969266@qq.com>
2021-03-26 15:53:32 +0800

    第七次commit
    使用sequelize创建数据库连接

--- 081800306&041802224/controllers/mysqlConfig.js
+++ 081800306&041802224/controllers/mysqlConfig.js
@@ -1,43 +1,28 @@
-//mysqlConfig.js
-var mysql = require('mysql');
-var config = require('./defaultConfig');
+const Sequelize = require("sequelize");
+const config = require("./config");
 
-var pool = mysql.createPool({
-    host: config.database.HOST,
-    user: config.database.USERNAME,
-    password: config.database.PASSWORD,
-    database: config.database.DATABASE
-});
+console.log('init sequelize...');
 
-let allServices = {
-    query: function (sql, values) {
-
-        return new Promise((resolve, reject) => {
-            pool.getConnection(function (err, connection) {
-                if (err) {
-                    reject(err)
-                } else {
-                    connection.query(sql, values, (err, rows) => {
-                        if (err) {
-                            reject(err)
-                        } else {
-                            resolve(rows)
-                        }
-                        connection.release()
-                    })
-                }
-            })
-        })
-
+const sequelize = new Sequelize(config.database, config.username, config.password, {
+    host: config.host, //数据库地址
+    dialect: 'mysql', //指定连接的数据库类型
+    pool: {
+        max: 5, //连接池最大连接数量
+        min: 0, //最小连接数量
+        idle: 10000, //如果一个线程 10秒内么有被使用过的话，就释放
     },
-   findUserData: function (name) {
-        let _sql = `select * from users where name="${name}";`
-        return allServices.query(_sql)
-    },
-    addUserData: (obj) => {
-         let _sql = "insert into users set name=?,pass=?,avator=?,moment=?;"
-         return allServices.query(_sql, obj)
-     },
-}
+    logging: true, // 执行过程会log一些SQL的logging，设为false不显示
+})
 
-module.exports = allServices;
+//对连接进行测试，查看控制台
+sequelize
+    .authenticate()
+    .then(() => {
+        console.log('******Connection has been established successfully.********');
+        console.log('******测试结束，即将退出！！！********');
+        process.exit(); //结束进程
+    })
+    .catch(err => {
+        console.error('***************Unable to connect to the database:***********', err);
+    });
+module.exports = sequelize;
---
+++ 081800306&041802224/models/keywords.js
@@ -0,0 +1,26 @@
+const Sequelize = require("sequelize");
+const sequelize = require('../controllers/mysqlConfigs');
+
+const Keywords = sequelize.define('keywords', {
+    academicNum: {
+        type: Sequelize.INTEGER(11),
+    },
+    keyword: Sequelize.STRING(100),
+    id: {
+        type: Sequelize.INTEGER(11),
+        PrimaryKey: true
+    }
+}, {
+    freezeTableName: false,
+    timestamps: true
+});
+//timestamp字段，默认为true，表示数据库中是否会自动更新createdAt和updatedAt字段，false表示不会增加这个字段。
+//freezeTableName,默认为true,会自动给表名表示为复数: user => users，为false则表示，使用我设置的表名
+
+
+//创建表，默认是false，true则是删除原有表，再创建
+User.sync({
+    force: false,
+});
+
+module.exports = User;
