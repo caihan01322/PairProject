265ade374c71f67b88a05bc4e458b35814f65b21
221801123 <1041618073@qq.com>
2021-03-28 23:50:43 +0800

    用户登录底层逻辑

--- 221801123&221801120/src/main/java/com/pair/service/impl/UserServiceImpl.java
+++ 221801123&221801120/src/main/java/com/pair/service/impl/UserServiceImpl.java
@@ -4,8 +4,10 @@ package com.pair.service.impl;
 import com.pair.dao.UserDao;
 import com.pair.model.role.User;
 import com.pair.service.UserService;
+import com.pair.util.DataUtil;
 import org.springframework.stereotype.Service;
 import javax.annotation.Resource;
+import java.util.List;
 
 @Service("userService")
 public class UserServiceImpl implements UserService {
@@ -14,6 +16,8 @@ public class UserServiceImpl implements UserService {
 
     @Override
     public User login(String name, String password) {
-        return null;
+        String sql = "select * from user where name = ? and password = ?";
+        List<User> users = userDao.queryBySQL(sql, new Object[] {name, password});
+        return DataUtil.isValid(users) ? users.get(0) : null;
     }
 }
