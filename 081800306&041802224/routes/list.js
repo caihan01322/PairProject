32a3f43d1e573a6053a6d66a75e9e91c536b4a1e
czj6 <673330108@qq.com>
2021-03-28 21:20:43 +0800

    论文查询完成

--- 081800306&041802224/routes/list.js
+++ 081800306&041802224/routes/list.js
@@ -16,4 +16,16 @@ router.get('/list', async (ctx) => {
   // ctx.body = articles;
   await ctx.render('list', { articles });
 });
+router.get('/getArticles', async (ctx) => {
+  let key = ctx.query.key; // eslint-disable-line
+  let page = ctx.query.page || 0;
+  let articles;
+  let list;
+  if (key) {
+    console.log(key);
+    list = await Article.getIdsByKey(key);
+    articles = await Article.getArticles(list, page);
+  }
+  ctx.body = articles;
+});
 module.exports = router;
