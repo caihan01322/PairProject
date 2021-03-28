c4ef24e9538a491dc28b79d123fbe82bccac869b
Lin <1220627779@qq.com>
2021-03-28 16:31:01 +0800

    搜索功能

--- paper/backend/views/paper/index.php
+++ paper/backend/views/paper/index.php
@@ -3,21 +3,16 @@
 use yii\helpers\Html;
 use yii\grid\GridView;
 
-/* @var $this yii\web\View */
-/* @var $searchModel common\models\PaperSearch */
-/* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Papers';
+$this->title = '论文管理';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="paper-index">
 
     <h1><?= Html::encode($this->title) ?></h1>
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <p>
-        <?= Html::a('Create Paper', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+    <h3>搜索</h3>
+    <?php  echo $this->render('_search', ['model' => $searchModel]); ?>
+  
     <?= GridView::widget([
         'dataProvider' => $dataProvider,
         'filterModel' => $searchModel,
