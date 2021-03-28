0264cd2e76afe0741fe5e966b6ae453a5735bbca
Lin <1220627779@qq.com>
2021-03-28 12:58:59 +0800

    查看论文

--- paper/backend/controllers/PaperController.php
+++ paper/backend/controllers/PaperController.php
@@ -44,6 +44,33 @@ class PaperController extends Controller
         ]);
     }
 
-   
+    /**
+     * Displays a single Paper model.
+     * @param string $id
+     * @return mixed
+     */
+    public function actionView($id)
+    {
+        return $this->render('view', [
+            'model' => $this->findModel($id),
+        ]);
+    }
+
     
+
+    /**
+     * Finds the Paper model based on its primary key value.
+     * If the model is not found, a 404 HTTP exception will be thrown.
+     * @param string $id
+     * @return Paper the loaded model
+     * @throws NotFoundHttpException if the model cannot be found
+     */
+    protected function findModel($id)
+    {
+        if (($model = Paper::findOne($id)) !== null) {
+            return $model;
+        } else {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+    }
 }
