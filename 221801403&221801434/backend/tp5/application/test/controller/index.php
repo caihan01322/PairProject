6c947d260dd4d76aa73bd61e86db8d1ab8d78169
Ye777 <58556652+Ye777@users.noreply.github.com>
2021-03-29 00:26:42 +0800

    返回前十个关键词及其对应数量

--- 221801403&221801434/backend/tp5/application/test/controller/index.php
+++ 221801403&221801434/backend/tp5/application/test/controller/index.php
@@ -52,6 +52,28 @@ public function searchkey($keyword)
     return $this->fetch('informshow');
 
 }
+//返回关键词和词数
+public function keywordandnum()
+{
+    $data=keywords::select();
+    
+    //$paper=M("paper");
+    //$paper->where('type=1 AND status=1')->select();
+    //$where['name']=array('like','jb51%');
+    //$userForm->where($where)->select();
+    //$data =paper::where("title","DeepPhys: Video-Based Physiological Measurement Using Convolutional Attention Networks")->select();
+    //return json($data);
+    
+    //模糊查询
+    
+    //$data=article::where('title like "%qr%"')->select();
+
+    //查询列
+    //$data=article::column('title');
+    $this->assign('income', $data);
+    return $this->fetch('statistic');
+
+}
 
 
 
