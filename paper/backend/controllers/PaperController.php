b4f8f35334522d907b2148bb04903eff4f9a000d
Lin <1220627779@qq.com>
2021-03-28 14:35:19 +0800

    修改文章

--- paper/backend/controllers/PaperController.php
+++ paper/backend/controllers/PaperController.php
@@ -56,7 +56,39 @@ class PaperController extends Controller
         ]);
     }
 
-    
+ 
+
+    /**
+     * Updates an existing Paper model.
+     * If update is successful, the browser will be redirected to the 'view' page.
+     * @param string $id
+     * @return mixed
+     */
+    public function actionUpdate($id)
+    {
+        $model = $this->findModel($id);
+
+        if ($model->load(Yii::$app->request->post()) && $model->save()) {
+            return $this->redirect(['view', 'id' => $model->link]);
+        } else {
+            return $this->render('update', [
+                'model' => $model,
+            ]);
+        }
+    }
+
+    /**
+     * Deletes an existing Paper model.
+     * If deletion is successful, the browser will be redirected to the 'index' page.
+     * @param string $id
+     * @return mixed
+     */
+    public function actionDelete($id)
+    {
+        $this->findModel($id)->delete();
+
+        return $this->redirect(['index']);
+    }
 
     /**
      * Finds the Paper model based on its primary key value.
