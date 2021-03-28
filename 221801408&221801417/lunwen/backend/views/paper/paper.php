879465b85353ca8d3d7fd69fb6d2369f40d11bfd
llk1233 <76471936+llk1233@users.noreply.github.com>
2021-03-28 20:23:44 +0800

    创建文章展示界面

---
+++ 221801408&221801417/lunwen/backend/views/paper/paper.php
@@ -0,0 +1,35 @@
+<?php
+
+use yii\helpers\Html;
+use yii\grid\GridView;
+
+/* @var $this yii\web\View */
+/* @var $searchModel common\models\PaperSearch */
+/* @var $dataProvider yii\data\ActiveDataProvider */
+
+$this->title = '论文列表';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="paper-index">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+
+    
+    <?= GridView::widget([
+        'dataProvider' => $dataProvider,
+        'filterModel' => $searchModel,
+        'columns' => [
+            ['class' => 'yii\grid\SerialColumn'],
+
+            'title',
+           // 'abstract:ntext',
+            'typeandyear',
+            'keywords',
+            'releasetime',
+            //'link',
+          
+            ['class' => 'yii\grid\ActionColumn'],
+        ],
+    ]); ?>
+</div>
