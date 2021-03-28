bbf167c22527f3b7cdbe5703717ff5b2ca05ff34
QAQWMZ <79703668+QAQWMZ@users.noreply.github.com>
2021-03-28 20:42:34 +0800

    增加了返回图表数据的基本方法

--- 021800623&111801206/backend/ArticleDao.java
+++ 021800623&111801206/backend/ArticleDao.java
@@ -6,6 +6,12 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 
 public class ArticleDao {
@@ -163,4 +169,98 @@ public static void deletePaper(int academicNum) {
 		}
 		
 	}
+
+public static ArrayList<Map.Entry<String,Integer>> selectYearData(String year,String meeting) {
+	
+	int min = 0;
+	int max = 0;
+	ArrayList<Paper> list = new ArrayList<Paper>();
+	if (meeting.equals("CVPR")) {
+		min = 1;
+		max = 1920;
+	}
+	else if (meeting.equals("ICCV")) {
+		min = 1921;
+		max = 2919;
+	}
+	else {
+		min = 2920;
+		max = 5951;
+	}
+	
+	String sqlString = "select * from academics where (year = '"+year+"') and  (academicNum >= "+min+") and (academicNum <="+max+")";
+	ResultSet rs = null;
+	
+	Connection connection = Base.getConn();
+	
+	PreparedStatement ps = null;
+	
+	try {
+		ps = connection.prepareStatement(sqlString);
+		rs = ps.executeQuery();
+		while(rs.next()) {
+			Paper paper = new Paper();
+			paper.setAcademicNum(rs.getInt("academicNum"));
+			paper.setAbstractString(rs.getString("abstract"));
+			paper.setLink(rs.getString("link"));
+			paper.setYear(rs.getString("year"));
+			paper.setTitle(rs.getString("title"));
+			ArrayList<String> keywordStrings = new ArrayList<String>();
+			sqlString = "select * from keywords where academicNum= '"+rs.getInt("academicNum")+"'";
+			PreparedStatement ps1 = connection.prepareStatement(sqlString);
+			ResultSet rs1 = ps1.executeQuery();
+			while(rs1.next()) {
+				keywordStrings.add(rs1.getString("keyword"));
+			}
+			paper.setKeywords(keywordStrings);
+			list.add(paper);
+		}
+	} catch (SQLException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+	List<Map.Entry<String, Integer>> wordList;
+	Map<String,Integer> wordMap = new HashMap<String, Integer>();
+	for (int i = 0; i < list.size(); i++) {
+		ArrayList<String> tempArrayList = list.get(i).getKeywords();
+		for (int j = 0; j < tempArrayList.size(); j++) {
+			String wordString = tempArrayList.get(j);
+			Integer countInteger = wordMap.get(wordString);
+			if (countInteger == null) {
+				countInteger = 1;
+			}
+			else {
+				countInteger++;
+			}
+			wordMap.put(wordString, countInteger);
+		}
+	}
+	
+	wordList = new ArrayList<Map.Entry<String,Integer>>(wordMap.entrySet());
+	Collections.sort(wordList, new Comparator<Map.Entry<String,Integer>>() {
+        @Override
+        public int compare(Map.Entry<String, Integer> o1, Map.Entry<String, Integer> o2) {
+            if (o2.getValue()==o1.getValue()){
+                return o1.getKey().compareTo(o2.getKey());
+            }
+            return o2.getValue().compareTo(o1.getValue());
+        }
+    });
+	
+	int i = 1;
+	Map<String,Integer> wordMap1 = new HashMap<String, Integer>();
+	for (Map.Entry<String,Integer> entry:wordList)
+	{
+		if (i>10) {
+			break;
+		}
+		wordMap1.put(entry.getKey(), entry.getValue());
+		i++;
+	}
+	ArrayList<Map.Entry<String, Integer>> wordList1 = new ArrayList<Map.Entry<String,Integer>>(wordMap1.entrySet());;
+	return wordList1;
+	
+}
+	
 }
