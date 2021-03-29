6b72a8b0b6cd1372b6d8228ee0a601584278a44b
zwh1426 <1515071283@qq.com>
2021-03-29 14:05:14 +0800

    Create view.php

---
+++ 221801425&221801415/lunwen/backend/views/Paper/view.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\DetailView;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Paper */
+
+$this->title = $model->title;
+$this->params['breadcrumbs'][] = ['label' => '论文管理', 'url' => ['index']];
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="paper-view">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <p>
+        <?= Html::a('修改', ['update', 'id' => $model->link], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a('删除', ['delete', 'id' => $model->link], [
+            'class' => 'btn btn-danger',
+            'data' => [
+                'confirm' => '你确定删除这篇论文吗?',
+                'method' => 'post',
+            ],
+        ]) ?>
+    </p>
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'attributes' => [
+            'title',
+            'abstract:ntext',
+            'typeandyear',
+            'keywords',
+            'releasetime',
+            'link',
+        ],
+    ]) ?>
+
+</div>
