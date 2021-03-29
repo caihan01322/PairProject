869bb9f8f7ac56ad13e4d2d71b147d95f6e80109
hpc <1132874992@qq.com>
2021-03-29 09:23:20 +0800

    用cookie传数据，跳转网页之后获取cookie中的数据

--- PairProjectDemo/tp5/application/index/controller/Edit.php
+++ PairProjectDemo/tp5/application/index/controller/Edit.php
@@ -10,10 +10,8 @@ class Edit extends Controller
     protected $request;
     public function index()
     {
-        
         $count = Db::table('paper')->count();
         $data = Db::table('paper')->select();
-        
         $this->assign('tableData',json_encode($data));
         $this->assign('count',$count);
         return $this->fetch();
--- PairProjectDemo/tp5/application/index/controller/EditDetail.php
+++ PairProjectDemo/tp5/application/index/controller/EditDetail.php
@@ -1,16 +1,11 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
+use think\Request;
 
 class EditDetail extends Controller{
+    
     public function index(){
-        $editDetail = [
-            "title"=>"abc",
-            "abstract"=>"abc",
-            "keyword"=>"abc",
-            "url"=>"abc"
-        ];
-        $this->assign("editPaper",json_encode($editDetail));
         return $this->fetch();
     }
 }
