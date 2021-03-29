d09f26c3975ba9e3a9e609a5247d87ad2c13173d
hpc <1132874992@qq.com>
2021-03-29 10:37:08 +0800

    编辑论文数据

--- PairProjectDemo/tp5/application/index/controller/EditDetail.php
+++ PairProjectDemo/tp5/application/index/controller/EditDetail.php
@@ -1,11 +1,44 @@
 <?php
 namespace app\index\controller;
 use think\Controller;
-use think\Request;
+use think\Db;
 
 class EditDetail extends Controller{
     
     public function index(){
         return $this->fetch();
+    }
+    public function updatePaper(){
+        $listString = $_COOKIE["updateList"];
+        
+        $list = json_decode($listString,true);
+
+        
+        $oldTitle =  $list["oldTitle"];
+        $title = $list["title"];
+        $oldLink = $list["oldLink"];
+        $link = $list["link"];
+        $oldAbstract = $list["oldAbstract"];
+        $abstract = $list["abstract"];
+        $oldKeyword = $list["oldKeyword"];
+        $keyword = $list["keyword"];
+        // $form = json_encode($_POST);
+        $result = Db::table('paper')
+            ->where("title",$oldTitle)
+            ->where("link",$oldLink)
+            ->where("abstract",$oldAbstract)
+            ->where("keyword",$oldKeyword)
+            ->update([
+                "title"=>$title,
+                "link"=>$link,
+                "abstract"=>$abstract,
+                "keyword"=>$keyword
+        ]);
+        if($result!==false){
+            return $this->success("编辑成功");
+        }
+        else {
+            return $this->error("编辑失败");
+        }
     }
 }
