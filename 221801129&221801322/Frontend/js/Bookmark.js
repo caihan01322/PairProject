2c2e6ce35df2ae5631e1c2d2e0406430477997b2
caihan <2249618917@qq.com>
2021-03-29 16:58:22 +0800

    收藏夹页面

---
+++ 221801129&221801322/Frontend/js/Bookmark.js
@@ -0,0 +1,16 @@
+$(function () {
+    var urlStr = "/Bookmark";
+    var uid = {
+        uid:localStorage.getItem("uid")
+    };
+    PostHandle(urlStr, JSON.stringify(uid), function(data){
+        //收藏夹结果接收与展示
+    });
+    
+    $(".markedSvg").click(function(){
+        alert("取消收藏！");
+        $(".markedSvg").attr("src","../img/Bookmark/mark.svg");
+        $(".markedSvg").attr("title","未收藏");
+        $(".markedSvg").css("cursor","default");
+    });
+})
