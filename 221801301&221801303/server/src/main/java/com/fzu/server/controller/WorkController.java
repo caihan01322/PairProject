68e4e480d6ed1ff41267c9bec7640b4d66bfde9b
6586744 <1035928314@qq.com>
2021-03-29 13:28:45 +0800

    [Fixed]作者名模糊查询

--- 221801301&221801303/server/src/main/java/com/fzu/server/controller/WorkController.java
+++ 221801301&221801303/server/src/main/java/com/fzu/server/controller/WorkController.java
@@ -4,42 +4,41 @@ import com.alibaba.fastjson.JSONObject;
 import com.fzu.server.service.WorkService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
 @Controller
+@CrossOrigin("*")
 public class WorkController {
     @Autowired
     WorkService ws;
-    @RequestMapping("/queryName")
+    @RequestMapping(value = "/queryName",method = {RequestMethod.POST})
     @ResponseBody
     public Object queryName(@RequestBody Map<String,Object> name){
         return ws.queryName(name);
     }
 
-    @RequestMapping("/queryAuthor")
+    @RequestMapping(value = "/queryAuthor",method = {RequestMethod.POST})
     @ResponseBody
     public Object queryAuthor(@RequestBody Map<String,Object> name){
         return ws.queryAuthor(name);
     }
 
-    @RequestMapping("/getCVPR")
+    @RequestMapping(value = "/getCVPR",method = {RequestMethod.GET})
     @ResponseBody
     public Object getCVPR(){
         return ws.getCVPR();
     }
 
-    @RequestMapping("/getECCV")
+    @RequestMapping(value = "/getECCV",method = {RequestMethod.GET})
     @ResponseBody
     public Object getECCV(){
         return ws.getECCV();
     }
 
-    @RequestMapping("/getICCV")
+    @RequestMapping(value = "/getICCV",method = {RequestMethod.GET})
     @ResponseBody
     public Object getICCV(){
         return ws.getICCV();
