1551af30591933d6c7e686c182b083ed8d03a3a4
kepachirenguai <79777840+kepachirenguai@users.noreply.github.com>
2021-03-29 23:17:28 +0800

    论文分析（第九次）

--- 221801409&221801418/application/index/controller/SearchInterface.php
+++ 221801409&221801418/application/index/controller/SearchInterface.php
@@ -1,23 +1,26 @@
 <?php
 
 namespace app\index\controller;
+
 use think\Controller;
 use think\Db;
 use think\db\connector\Mysql;
 
 class SearchInterface extends Controller
 {
-    public function create(){
+    public function create()
+    {
         $Db = new Db;
         $data  = Db::table('paper')->select();
-        $haha=json_encode($data);
-        $this->assign('try',$haha);
+        $haha = json_encode($data);
+        $this->assign('try', $haha);
         return view('SearchInterface');
     }
 
-    public function getNoModelData(){
+    public function getNoModelData()
+    {
         $Db = new Db;
         $data = Db::table('paper')->select();
-        $this->assign("user",$data);
+        $this->assign("user", $data);
     }
 }
