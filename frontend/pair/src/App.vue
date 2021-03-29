cedb32d6737bad5afad06852f1883458d69ba3c6
Kosmos5 <1821748823@qq.com>
2021-03-29 23:07:24 +0800

    添加登录页面
    添加登录页面

--- frontend/pair/src/App.vue
+++ frontend/pair/src/App.vue
@@ -1,16 +1,49 @@
 <template>
   <div id="app">
-    <Test></Test>
+    <el-container v-if="$route.meta.keepalive">
+      <el-header class="header">
+        <keep-alive>
+          <v_Header></v_Header>
+        </keep-alive>
+      </el-header>
+    <el-container>
+    <el-aside style="width:200px; height:600px">
+      <keep-alive>
+        <v-leftmenu></v-leftmenu>
+      </keep-alive>
+    </el-aside>
+    <el-container>
+      <el-main>
+        <keep-alive>
+          <router-view></router-view>
+        </keep-alive>
+      </el-main>
+      <el-footer style=" padding-left: 0px;padding-right: 0px;">
+        <v_footer></v_footer>
+      </el-footer>
+    </el-container>
+    </el-container>
+    </el-container>
+    <router-view v-if="!$route.meta.keepalive"></router-view>
+    <!-- <Test></Test>
     <img src="./assets/logo.png">
-    <router-view/>
+    </router-view>  -->
   </div>
 </template>
 
 <script>
-import Test from './components/Test.vue'
+import LeftMenu from './components/LeftMenu'
+import Header from './components/Header'
+import Footer from './components/Footer'
+// import Test from './components/Test.vue'
 export default {
-  components: { Test },
-  name: 'App'
+  // components: { Test },
+  name: 'App',
+  components: {
+    'v_Header': Header,
+    'v_leftmenu': LeftMenu,
+    'v_footer': Footer
+  }
 }
 </script>
 
@@ -21,6 +54,12 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  /* margin-top: 60px; */
 }
+
+  .header{
+    line-height: 60px;
+    background-color: #545c64;
+    /* text-color: white; */
+  }
 </style>
--- frontend/pair/src/router/index.js
+++ frontend/pair/src/router/index.js
@@ -1,15 +1,33 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import login from '@/components/Login'
+import Main from '@/components/Main'
+// import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    // {
+    //   path: '/',
+    //   name: 'HelloWorld',
+    //   component: HelloWorld
+    // },
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'login',
+      components: login,
+      meta: {
+        keepalive: false
+      }
+    },
+    {
+      path: '/Main',
+      name: 'Main',
+      compnent: Main,
+      meta: {
+        keepalive: true
+      }
     }
   ]
 })
