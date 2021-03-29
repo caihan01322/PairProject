9faaa9cb6bafb9bdaf56d16d3c905b85757793f6
1069042059 <1069042059@qq.com>
2021-03-29 19:39:31 +0800

    对keyword.dao的使用

---
+++ 221801307&&221801327/demo_01/src/main/java/com/lz/demo_01/service/KeywordsServiceImpl.java
@@ -0,0 +1,18 @@
+package com.lz.demo_01.service;
+
+import com.lz.demo_01.dao.KeywordsDao;
+import com.lz.demo_01.pojo.Keywords;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class KeywordsServiceImpl implements KeywordsService{
+    @Autowired
+    private KeywordsDao keywordsdao;
+    public List<Keywords> findAllKeywords(){ return keywordsdao.findAllKeywords();};
+    public List<Keywords> findAllKeywordsCVPR(){return keywordsdao.findAllKeywordsCVPR();}
+    public List<Keywords> findAllKeywordsICCV(){return keywordsdao.findAllKeywordsICCV();}
+    public List<Keywords> findAllKeywordsECCV(){return keywordsdao.findAllKeywordsECCV();}
+}
