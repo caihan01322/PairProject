5b70b8705aa4835513973d8199530658174376e9
llk1233 <76471936+llk1233@users.noreply.github.com>
2021-03-29 12:37:21 +0800

    增加论文表单界面

---
+++ 221801408&221801417/lunwen/backend/views/paper/_form.php
@@ -0,0 +1,33 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model common\models\Paper */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="paper-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'abstract')->textarea(['rows' => 6]) ?>
+
+    <?= $form->field($model, 'typeandyear')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'keywords')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'releasetime')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'link')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton($model->isNewRecord ? '爬取' : '修改', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
