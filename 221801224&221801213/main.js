dffd40cc778764451e3208552fd4e05eb4b04e87
Guodd-1 <1563854942@qq.com>
2021-03-31 01:55:19 +0800

    修改查询结果显示效果

--- 221801224&221801213/main.js
+++ 221801224&221801213/main.js
@@ -1,36 +1,3 @@
-// var searchBar = new Vue({
-//     el: "#searchBar",
-//     data: {
-//         joke: "一个笑话",
-
-//     },
-//     methods: {
-//         // testfunc: function() {
-//         //     var that = this;
-//         //     axios.get('http://localhost:5000/pair/api/v1/paper/search?' +
-//         //             'page=2&key=a&token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjoxLCJleHAiOjEwMDAwMDAwMDAwMDAwMDAwMTU5MDI4OTExMDk3NTk5MTgwNDY4MzYwODA4NTYzOTQ1MjgxMzg5NzgxMzI3NTU3NzQ3ODM4NzcyMTcwMzgxMDYwODEzNDY5OTg1ODU2ODE1MTA0fQ.fOFAk2NIOfaQMam_QmrNFtghOyto_7eA6tZ4O_uIams')
-//         //         .then(function(response) {
-//         //             console.log(response);
-//         //             that.joke = response.data.Data[1].Title;
-//         //         })
-//         //         .catch(function(err) {})
-
-//         // }
-
-//         testfunc: function() {
-//             var that = this;
-//             axios.get('https://api.gugudata.com/news/joke/demo')
-//                 .then(function(response) {
-//                     that.joke = response.data.Data[1].Title;
-//                     resultBar.data.listData[0].heading = response.data.Data[1].Title;
-//                 })
-//                 .catch(function(err) {})
-
-//         }
-//     }
-
-// })
-
 var rightsideBar = new Vue({
     el: "#rightsideBar",
     data: {
@@ -47,19 +14,39 @@ var rightsideBar = new Vue({
 
 var main = new Vue({
     el: "#main",
-    data: {
-        pageNum: 10,
-        pageNow: 1,
-        listData: [
-            { num: 1, heading: "标题1", content: "内容1", keyword1: "这是一个关键词", source: "http://www.baidu.com" },
-            { num: 2, heading: "标题2", content: "内容2", keyword1: "这是一个关键词", source: "http://www.bilibili.com" },
-            { num: 3, heading: "标题3", content: "内容3", keyword1: "这是一个关键词", source: "http://www.baidu.com" },
-            { num: 4, heading: "标题4", content: "内容4", keyword1: "这是一个关键词", source: "http://www.baidu.com" },
-            { num: 5, heading: "标题5", content: "内容5", keyword1: "这是一个关键词", source: "http://www.baidu.com" },
-            { num: 6, heading: "标题6", content: "内容6", keyword1: "这是一个关键词", source: "http://www.baidu.com" }
-        ],
-        userInput: "",
+    // data: {
+    //     pageNum: 10,
+    //     pageNow: 1,
+    //     listData: [
+    //         { num: 1, heading: "标题1", content: "内容1", keyword1: "这是一个关键词", source: "http://www.baidu.com" },
+    //         { num: 2, heading: "标题2", content: "内容2", keyword1: "这是一个关键词", source: "http://www.bilibili.com" },
+    //         { num: 3, heading: "标题3", content: "内容3", keyword1: "这是一个关键词", source: "http://www.baidu.com" },
+    //         { num: 4, heading: "标题4", content: "内容4", keyword1: "这是一个关键词", source: "http://www.baidu.com" },
+    //         { num: 5, heading: "标题5", content: "内容5", keyword1: "这是一个关键词", source: "http://www.baidu.com" },
+    //         { num: 6, heading: "标题6", content: "内容6", keyword1: "这是一个关键词", source: "http://www.baidu.com" }
+    //     ],
+    //     userInput: "",
+    //     isShow: false,
+    // },
+
+    data() {
+        return {
+            info: [],
+            pageNum: 10,
+            pageNow: 1,
+            listData: [
+                { num: 1, heading: "标题1", content: "内容1", keyword1: "这是一个关键词", source: "http://www.baidu.com" },
+                { num: 2, heading: "标题2", content: "内容2", keyword1: "这是一个关键词", source: "http://www.bilibili.com" },
+                { num: 3, heading: "标题3", content: "内容3", keyword1: "这是一个关键词", source: "http://www.baidu.com" },
+                { num: 4, heading: "标题4", content: "内容4", keyword1: "这是一个关键词", source: "http://www.baidu.com" },
+                { num: 5, heading: "标题5", content: "内容5", keyword1: "这是一个关键词", source: "http://www.baidu.com" },
+                { num: 6, heading: "标题6", content: "内容6", keyword1: "这是一个关键词", source: "http://www.baidu.com" }
+            ],
+            userInput: "",
+            isShow: false,
+        }
     },
+
     methods: {
 
         doSearching() {
@@ -67,6 +54,7 @@ var main = new Vue({
             //向后端发送最新的userInput和page=1
             this.pageNow = 1;
             this.update();
+            this.isShow = true;
         },
 
         pageUp: function() {
@@ -103,6 +91,17 @@ var main = new Vue({
             }
 
         },
-    }
+
+        t1: function() {
+            axios.get("./testdataL.json").then(response => (this.info = response.data.data));
+            console.log(this.info);
+        },
+    },
+
+
+
+
+
+
 
 })
