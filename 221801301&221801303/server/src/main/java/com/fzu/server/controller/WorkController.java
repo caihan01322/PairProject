0b10df07683bdee3abb909c08d7d9cecc557d84c
6586744 <1035928314@qq.com>
2021-03-31 00:45:39 +0800

    [Added]注释

--- 221801301&221801303/server/src/main/java/com/fzu/server/controller/WorkController.java
+++ 221801301&221801303/server/src/main/java/com/fzu/server/controller/WorkController.java
@@ -17,48 +17,56 @@ public class WorkController {
 
     @RequestMapping(value = "/queryName",method = {RequestMethod.POST})
     @ResponseBody
+    //通过文章标题查询(可以模糊查询)
     public Object queryName(@RequestBody Map<String,Object> req){
         return ws.queryName(req);
     }
 
     @RequestMapping(value = "/queryAuthor",method = {RequestMethod.POST})
     @ResponseBody
+    //通过文章作者名查询(可以模糊查询)
     public Object queryAuthor(@RequestBody Map<String,Object> req){
         return ws.queryAuthor(req);
     }
 
     @RequestMapping(value = "/queryKeyword",method = {RequestMethod.POST})
     @ResponseBody
+    //通过关键词查询(不支持模糊查询)
     public Object queryKeyword(@RequestBody Map<String,Object> req){
         return ws.queryKeyword(req);
     }
 
     @RequestMapping(value = "/getCVPR",method = {RequestMethod.GET})
     @ResponseBody
+    //获取CVPR会议TOP10
     public Object getCVPR(){
         return ws.getCVPR();
     }
 
     @RequestMapping(value = "/getECCV",method = {RequestMethod.GET})
     @ResponseBody
+    //获取ECCV会议TOP10
     public Object getECCV(){
         return ws.getECCV();
     }
 
     @RequestMapping(value = "/getICCV",method = {RequestMethod.GET})
     @ResponseBody
+    //获取ICCV会议TOP10
     public Object getICCV(){
         return ws.getICCV();
     }
 
     @RequestMapping(value = "/getTOP",method = {RequestMethod.GET})
     @ResponseBody
+    //获取所有会议TOP10
     public Object getTOP(){
         return ws.getTOP();
     }
 
     @RequestMapping(value = "/getDetail",method = {RequestMethod.POST})
     @ResponseBody
+    //通过文章id查询
     public Object getDetail(@RequestBody Map<String,Object> req){
         return ws.getDetail(req);
     }
--- 221801301&221801303/server/src/main/java/com/fzu/server/dao/PaperDao.java
+++ 221801301&221801303/server/src/main/java/com/fzu/server/dao/PaperDao.java
@@ -8,6 +8,7 @@ import java.util.Map;
 
 @Mapper
 public interface PaperDao {
+    //读入数据库
     void addECCVPaper(Paper paper);
     void addECCVKeyword(int pID,String keyword);
 
@@ -23,17 +24,17 @@ public interface PaperDao {
     void addKeyword(int pID,String keyword);
     void addAuthor(int pID,String author);
 
-    List<Paper> getPaperByName(String name,int start,int lim);
-    List<Paper> getPaperByAuthor(String author,int start,int lim);
-    List<Paper> getPaperByKeyword(String keyword,int start,int lim);
-    int getCount(String str,int state);
-    Paper getDetail(int id);
+    List<Paper> getPaperByName(String name,int start,int lim);//通过文章名查询
+    List<Paper> getPaperByAuthor(String author,int start,int lim);//通过作者名查询
+    List<Paper> getPaperByKeyword(String keyword,int start,int lim);//通过关键词查询
+    int getCount(String str,int state);//获取查询的总数(state:0为文章名查询，1为作者，2为关键词)
 
-    List<String> getKeyword(Integer id);
-    List<String> getAuthor(Integer id);
+    Paper getDetail(int id);//通过id查询
+    List<String> getKeyword(Integer id);//通过id获得keyword
+    List<String> getAuthor(Integer id);//通过id获得author
 
-    List<Map<String,String>> getCVPR();
-    List<Map<String,String>> getECCV();
-    List<Map<String,String>> getICCV();
-    List<Map<String,String>> getTOP();
+    List<Map<String,String>> getCVPR();//获得CVPR会议TOP10
+    List<Map<String,String>> getECCV();//获得ECCV会议TOP10
+    List<Map<String,String>> getICCV();//获得ICCV会议TOP10
+    List<Map<String,String>> getTOP();//获取全部会议TOP10
 }
