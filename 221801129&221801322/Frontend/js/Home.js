a51fb7652752d71c21d6ad979707cbdd01015999
caihan <2249618917@qq.com>
2021-03-30 19:37:28 +0800

    显示文件上传

--- 221801129&221801322/Frontend/js/Home.js
+++ 221801129&221801322/Frontend/js/Home.js
@@ -2,12 +2,26 @@ $(function () {
     $("#status").click(function(){
         $("#login").fadeIn("slow");
     });
+
     $(document).bind("click", function (e) {
         var target = $(e.target);
         if (target.closest("#status").length == 0 && target.closest("#login").length == 0) {
             $("#login").fadeOut("slow");
         }
     })
+
+    $("#upload").click(function(){
+        var inputObj=document.createElement('input');
+        inputObj.setAttribute('id','file');
+        inputObj.setAttribute('type','file');
+        inputObj.setAttribute('name','file');
+        inputObj.setAttribute("style",'visibility:hidden');
+        $("body").append(inputObj);
+        //inputObj.value;
+        inputObj.click();
+        //console.log(inputObj);
+    })
+
     $("#loginBtn").click(function(){
         if($("user").val() == ""){
             alert("请输入用户名");
