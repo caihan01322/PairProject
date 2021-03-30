fc77f72d0c8dbb05ff12a3dcdb507657b9043ae4
weihaochen111 <786576329@qq.com>
2021-03-30 19:33:12 +0800

    搜索关键词

--- 221801421_221801404/back_end/flask_demo.py
+++ 221801421_221801404/back_end/flask_demo.py
@@ -10,6 +10,7 @@ def name():
     db = Mysql()
     keyword = request.args.get('keyword')
     items = db.getItems(keyword)
+    key = db.getECCV()
     page_range = range(int(page) - 3, int(page) + 2)
     if int(page) < 4:
         page_range = range(1, int(page) + 4)
@@ -33,6 +34,23 @@ class Mysql(object):
                 self.cursor.execute(sql)
                 items = self.cursor.fetchall()
                 return  items
+
+            def getECCV(self):
+                global ECCV2016
+                global ECCV2018
+                global ECCV2020
+                global keyECCV
+                sql1 = "SELECT keyword FROM paper where typeandyear='ECCV 2016' "
+                sql2 = "SELECT keyword FROM paper where typeandyear='ECCV 2018' "
+                sql3 = "SELECT keyword FROM paper where typeandyear='ECCV 2020' "
+                self.cursor.execute(sql1)
+                datalist = []
+                alldata = self.cursor.fetchall()
+                for s in alldata:
+                    if s[0] is not None:
+                        datalist.append(s[0])
+                        str = ''.split(datalist) #将元组列表转化成字符串
+                        return str
 
 if __name__ == '__main__':
 app.run(app.run(debug=True))
