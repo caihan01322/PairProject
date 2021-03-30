f96f7e284bb82f7ebad42354083e7895e25abe73
QAQWMZ <79703668+QAQWMZ@users.noreply.github.com>
2021-03-30 20:24:21 +0800

    修改selectYearData()

--- 021800623&111801206/backend/ArticleDao.java
+++ 021800623&111801206/backend/ArticleDao.java
@@ -166,11 +166,12 @@ public static void deletePaper(int academicNum) {
 		
 	}
 
-public static ArrayList<Map.Entry<String,Integer>> selectYearData(String year,String meeting) {
+public static HotWord selectYearData(String year,String meeting) {
 	
-	int min = 0;
 	int max = 0;
-	ArrayList<Paper> list = new ArrayList<Paper>();
+	int min = 0;
+	
+	HotWord hotWord = new HotWord();
 	if (meeting.equals("CVPR")) {
 		min = 1;
 		max = 1920;
@@ -184,9 +185,11 @@ public static ArrayList<Map.Entry<String,Integer>> selectYearData(String year,St
 		max = 5951;
 	}
 	
-	String sqlString = "select * from academics where (year = '"+year+"') and  (academicNum >= "+min+") and (academicNum <="+max+")";
+	String sqlString = "select keyword from keywords where academicNum in "
+			+ "	(select academicNum from academics where year ='"+year+"' and academicNum <= "+max+" and academicNum>="+min+")";
 	ResultSet rs = null;
-	
+	ArrayList<String> keywordList = new ArrayList<String>();
+
 	Connection connection = Base.getConn();
 	
 	PreparedStatement ps = null;
@@ -195,45 +198,29 @@ public static ArrayList<Map.Entry<String,Integer>> selectYearData(String year,St
 		ps = connection.prepareStatement(sqlString);
 		rs = ps.executeQuery();
 		while(rs.next()) {
-			Paper paper = new Paper();
-			paper.setAcademicNum(rs.getInt("academicNum"));
-			paper.setAbstractString(rs.getString("abstract"));
-			paper.setLink(rs.getString("link"));
-			paper.setYear(rs.getString("year"));
-			paper.setTitle(rs.getString("title"));
-			ArrayList<String> keywordStrings = new ArrayList<String>();
-			sqlString = "select * from keywords where academicNum= '"+rs.getInt("academicNum")+"'";
-			PreparedStatement ps1 = connection.prepareStatement(sqlString);
-			ResultSet rs1 = ps1.executeQuery();
-			while(rs1.next()) {
-				keywordStrings.add(rs1.getString("keyword"));
-			}
-			paper.setKeywords(keywordStrings);
-			list.add(paper);
+			String keywordString = rs.getString("keyword");
+			keywordList.add(keywordString.toLowerCase());
+
 		}
 		connection.close();
 	} catch (SQLException e) {
 		// TODO Auto-generated catch block
 		e.printStackTrace();
 	}
-	
 	List<Map.Entry<String, Integer>> wordList;
 	Map<String,Integer> wordMap = new HashMap<String, Integer>();
-	for (int i = 0; i < list.size(); i++) {
-		ArrayList<String> tempArrayList = list.get(i).getKeywords();
-		for (int j = 0; j < tempArrayList.size(); j++) {
-			String wordString = tempArrayList.get(j);
-			Integer countInteger = wordMap.get(wordString);
-			if (countInteger == null) {
-				countInteger = 1;
-			}
-			else {
-				countInteger++;
-			}
-			wordMap.put(wordString, countInteger);
+	for (int i = 0; i < keywordList.size(); i++) {
+		String keywordString = keywordList.get(i);
+		Integer countInteger = wordMap.get(keywordString);
+		if (countInteger == null) {
+			countInteger = 1;
 		}
+		else {
+			countInteger++;
+		}
+		wordMap.put(keywordString, countInteger);
 	}
-	
+	hotWord.setWordMap(wordMap);
 	wordList = new ArrayList<Map.Entry<String,Integer>>(wordMap.entrySet());
 	Collections.sort(wordList, new Comparator<Map.Entry<String,Integer>>() {
         @Override
@@ -244,24 +231,25 @@ public static ArrayList<Map.Entry<String,Integer>> selectYearData(String year,St
             return o2.getValue().compareTo(o1.getValue());
         }
     });
-	
+	ArrayList<String> wordStrings = new ArrayList<String>();
+	ArrayList<Integer> numList = new ArrayList<Integer>();
 	int i = 1;
-	Map<String,Integer> wordMap1 = new HashMap<String, Integer>();
 	for (Map.Entry<String,Integer> entry:wordList)
 	{
 		if (i>10) {
 			break;
 		}
-		wordMap1.put(entry.getKey(), entry.getValue());
+		wordStrings.add(entry.getKey());
+		numList.add(entry.getValue());
 		i++;
 	}
-	ArrayList<Map.Entry<String, Integer>> wordList1 = new ArrayList<Map.Entry<String,Integer>>(wordMap1.entrySet());;
-	return wordList1;
+	hotWord.setNumList(numList);
+	hotWord.setWordList(wordStrings);
+	return hotWord;
+	
 }
 	
 
-
-	
 public static ArrayList<String> selectKeyWords() {
 	ArrayList<String> keywordList = new ArrayList<String>();
 	ResultSet rs = null;
@@ -277,6 +265,7 @@ public static ArrayList<String> selectKeyWords() {
 			String keywordString = rs.getString("keyword");
 			keywordList.add(keywordString.toLowerCase());
 		}
+		connection.close();
 		
 	} catch (SQLException e) {
 		// TODO Auto-generated catch block
