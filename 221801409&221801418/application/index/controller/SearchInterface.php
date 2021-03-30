cd674bc965386fd66f7f94a9aefbdfb9e1689636
kepachirenguai <79777840+kepachirenguai@users.noreply.github.com>
2021-03-30 13:05:59 +0800

    论文分析到论文搜索的跳转

--- 221801409&221801418/application/index/controller/SearchInterface.php
+++ 221801409&221801418/application/index/controller/SearchInterface.php
@@ -10,17 +10,9 @@ class SearchInterface extends Controller
 {
     public function create()
     {
-        $Db = new Db;
         $data  = Db::table('paper')->select();
         $haha = json_encode($data);
         $this->assign('try', $haha);
         return view('SearchInterface');
-    }
-
-    public function getNoModelData()
-    {
-        $Db = new Db;
-        $data = Db::table('paper')->select();
-        $this->assign("user", $data);
     }
 }
