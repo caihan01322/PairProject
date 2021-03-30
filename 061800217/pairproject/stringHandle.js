5218ed7b0c69304212bc874102fe42de9432cb79
liuruij <958177341@qq.com>
2021-03-31 07:32:41 +0800

    位置更新

---
+++ 061800217/pairproject/stringHandle.js
@@ -0,0 +1,24 @@
+function stringHan(str)//处理字符串
+{
+    let reg = new RegExp("\"","g");
+    let c = str.replace(reg,"");
+    let newStr = c.replace(/\[|]/g,'');    
+    return newStr;
+}
+function stringHandleYear(str)//处理字符串
+{
+    let newStr = stringHandle(str);   
+    let d = new Array();
+    d = newStr.split(" ");         
+    return d;
+}
+function stringSplit(str){//处理原表中的year列
+    let words = new Array();
+    words = str.split(" ");
+    return words;
+}
+module.exports = {
+    stringHan,
+    stringHandleYear,
+    stringSplit
+}
