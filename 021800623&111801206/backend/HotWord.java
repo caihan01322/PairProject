bbcae1c6a3f189c23813cb426d64e64126d682fc
QAQWMZ <79703668+QAQWMZ@users.noreply.github.com>
2021-03-30 19:56:44 +0800

    修改HotWord类

--- 021800623&111801206/backend/HotWord.java
+++ 021800623&111801206/backend/HotWord.java
@@ -1,32 +1,52 @@
 package test;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HotWord {
-	private String wordString;
-	private ArrayList<Integer> wordList;
+	private String meetingName;
+	private Map<String,Integer> wordMap;
+	private ArrayList<Integer> numList;
+	private ArrayList<String> wordList;
 	
-	public String getWordString() {
-		return wordString;
+	public ArrayList<Integer> getNumList() {
+		return numList;
 	}
-	public void setWordString(String wordString) {
-		this.wordString = wordString;
+
+	public void setNumList(ArrayList<Integer> numList) {
+		this.numList = numList;
 	}
-	public ArrayList<Integer> getWordList() {
+
+	public ArrayList<String> getWordList() {
 		return wordList;
 	}
-	
-	
-	public void setWordList(ArrayList<Integer> wordList) {
+
+	public void setWordList(ArrayList<String> wordList) {
 		this.wordList = wordList;
 	}
+
+	public String getMeetingName() {
+		return meetingName;
+	}
+
+	public void setMeetingName(String meetingName) {
+		this.meetingName = meetingName;
+	}
+
+	public Map<String, Integer> getWordMap() {
+		return wordMap;
+	}
+
+	public void setWordMap(Map<String, Integer> wordMap) {
+		this.wordMap = wordMap;
+	}
+	
 	public HotWord() {
-		this.wordString = null;
-		this.wordList = new ArrayList<Integer>();
-		wordList.add(0);
-		wordList.add(0);
-		wordList.add(0);
-		wordList.add(0);
+		this.meetingName = null;
+		this.wordMap = new HashMap<String,Integer>();
+		this.numList = new ArrayList<Integer>();
+		this.wordList = new ArrayList<String>();
 	}
 	
 
