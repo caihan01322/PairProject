3f1a2f4d0940e4e9fd85487fa08eb153aeaa2ee5
destinylemon <60696580+destinylemon@users.noreply.github.com>
2021-03-31 00:51:31 +0800

    github没办法修改文件名，只能本地改名提交新文件

---
+++ 221801304&221801331/main/java/cn/zcx/pa/PaperShow/pojo/KeywordCount.java
@@ -0,0 +1,15 @@
+package cn.zcx.pa.PaperShow.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class KeywordCount
+{
+  private String keyword;
+
+  private int count;
+}
