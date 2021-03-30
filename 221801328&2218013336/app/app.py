8b321d0c0cb528fddd0c57fa1b4fa1dd0508ba61
陈雨虹 <58587432+mayrain-yh@users.noreply.github.com>
2021-03-30 10:42:13 +0800

    json导入数据库

--- 221801328&2218013336/app/app.py
+++ 221801328&2218013336/app/app.py
@@ -89,5 +89,47 @@ def user_register():
     db.session.commit()
     return jsonify({'status':'ok'})
 
+#论文导入ECCV
+@app.route('/paper/add',methods=['POST'])
+def addPaper():
+    path = "C:\paper"
+    fileList = listdir(path)
+    fileIndex = []
+    for i in range(0,len(fileList)):
+        index = fileList[i].split(".json")[0]
+        path1 = path + '\\' + index + '.json'
+        resolveJson(path1)
+    return jsonify({"status":"ok"})
+
+def judge(key,Json):
+    if (key in Json):
+        return True
+    else:
+        return False
+
+#ECCV
+def resolveJson(path):
+    file = open(path,'rb')
+    paper = Paper()
+    fileJson = json.loads(file.read().decode('utf-8'))
+    abstract = fileJson["摘要"]
+    keyword = fileJson["关键词"]
+    key = ",".join(map(str,keyword))
+    datetime = fileJson["发布时间"]
+    href = fileJson["原文链接"]
+    title = fileJson["论文名称"]
+
+    paper.abstract = abstract
+    paper.keyWord = key
+    paper.datetime = datetime
+    paper.href = href
+    paper.title = title
+    paper.classify = 'ECCV'
+    db.session.add(paper)
+    db.session.commit()
+
+    file.close()
+    return jsonify({"status":"ok"})
+
 if __name__ == '__main__':
     app.run(debug=True)
