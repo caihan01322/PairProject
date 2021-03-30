ed5bd43e626a6c7ac34e916dc1246a103e77070b
unoni <523976222@qq.com>
2021-03-30 19:38:29 +0800

    实现了论文查询页面,并实现了后端将返回的json数据的解析

--- 221801421_221801404/front_end/src/components/ShowList.vue
+++ 221801421_221801404/front_end/src/components/ShowList.vue
@@ -1,13 +1,100 @@
 <template>
   <div class="main">
-  123
+    <div id="search">
+      <input type="text" name="infoInput" id="info" v-model="content" placeholder="请输入要查询的论文题目、ID或关键词"/>
+      <el-button type="primary" id="send" @click="getSearchInfo">查询</el-button>
+    </div>
+    <el-main>
+      <el-table
+        :data="tableData"
+        @row-click="showDetail"
+        :row-style="{height: '60px'}"
+        >
+        <el-table-column prop="title" label="标题" width="160">
+        </el-table-column>
+        <el-table-column prop="meeting" label="所属会议" width="100">
+        </el-table-column>
+        <el-table-column prop="postDate" label="发布时间" width="100">
+        </el-table-column>
+        <el-table-column prop="keyword" label="关键字" width="230" :show-overflow-tooltip='true'>
+        </el-table-column>
+        <el-table-column prop="abstractContext" label="摘要" :show-overflow-tooltip='true'>
+        </el-table-column>
+      </el-table>
+    </el-main>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+export default {
+  name: 'ShowList',
+  data () {
+    return {
+     tableData: '',
+     typeandyear:'',
+     selectedObj:''
+    }
+  },
+  methods: {
+    getSearchInfo () {
+      axios.get('../static/users.json').then((res) => {
+        this.tableData = res.data
+        //console.log(this.tableData)
+        this.splitTypeAndYear()
+      })
+    },
+
+    splitTypeAndYear(){
+      for (var i = 0; i < this.tableData.length; i++) {
+        this.typeandyear=this.tableData[i].typeandyear
+        this.$set(this.tableData[i],'meeting',this.meeting)
+        this.$set(this.tableData[i],'postDate',this.year)
+      }
+    },
+
+    tiaozhuan(){
+      this.$router.push('/PaperDetail')
+    },
+
+    showDetail(row, column, event) {
+      this.selectedObj=row
+      let str=JSON.stringify(this.selectedObj)
+      this.$router.push({path:'PaperDetail',query:{selectedObj:str}})
+    }
+  },
+
+  computed:{
+    meeting:function(){
+      return this.typeandyear.match("[a-zA-z]+")[0];
+    },
+    year:function(){
+      return this.typeandyear.match("[0-9]+")[0];
+    }
+  },
+
+  mounted() {
+    //初始返回全部论文,最后参数应该是1," "
+    this.getSearchInfo()
+  }
+}
 
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
 </style>
