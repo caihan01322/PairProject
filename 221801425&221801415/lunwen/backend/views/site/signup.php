c161bd2a559d339d3cacc99e09b3fdea45b162f0
zfy <1595601643@qq.com>
2021-03-30 22:17:11 +0800

    Update signup.php

--- 221801425&221801415/lunwen/backend/views/site/signup.php
+++ 221801425&221801415/lunwen/backend/views/site/signup.php
@@ -7,7 +7,7 @@
 use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
 
-$this->title = 'Signup';
+$this->title = '注册';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-signup">
@@ -20,8 +20,6 @@ $this->params['breadcrumbs'][] = $this->title;
             <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
 
                 <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-
-                <?= $form->field($model, 'email') ?>
 
                 <?= $form->field($model, 'password')->passwordInput() ?>
 
