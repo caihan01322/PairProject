56fe572f74c16cfd122759335bbf80cc9d3b5ee5
GaryChan029 <garychan929@ymail.com>
2021-03-31 10:10:11 +0800

    [Added] 图表页面

--- 221801238&221801210/frontend/src/main.js
+++ 221801238&221801210/frontend/src/main.js
@@ -4,9 +4,13 @@ import router from './router'
 import store from './store'
 
 //引入element-ui
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-Vue.use(ElementUI);
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI)
+
+//引入echarts
+import * as echarts from 'echarts'
+Vue.prototype.$echarts = echarts
 
 Vue.config.productionTip = false
 
