a8be2116c10c5f225a845eb5bcb1f2ba666d78ed
Guodd-1 <1563854942@qq.com>
2021-03-31 11:41:53 +0800

    修改实现前端分页

--- 221801224&221801213/main.js
+++ 221801224&221801213/main.js
@@ -1,67 +1,49 @@
-var rightsideBar = new Vue({
-    el: "#rightsideBar",
-    data: {
-        hotWordArr: ["机器学习", "机器学习", "机器学习", "机器学习", "机器学习",
-            "机器学习", "机器学习", "机器学习", "机器学习", "机器学习"
-        ],
-
-    },
-    methods: {
-        func: function() {}
-    }
-
-})
-
 var main = new Vue({
-    el: "#main",
-    // data: {
-    //     pageNum: 10,
-    //     pageNow: 1,
-    //     listData: [
-    //         { num: 1, heading: "标题1", content: "内容1", keyword1: "这是一个关键词", source: "http://www.baidu.com" },
-    //         { num: 2, heading: "标题2", content: "内容2", keyword1: "这是一个关键词", source: "http://www.bilibili.com" },
-    //         { num: 3, heading: "标题3", content: "内容3", keyword1: "这是一个关键词", source: "http://www.baidu.com" },
-    //         { num: 4, heading: "标题4", content: "内容4", keyword1: "这是一个关键词", source: "http://www.baidu.com" },
-    //         { num: 5, heading: "标题5", content: "内容5", keyword1: "这是一个关键词", source: "http://www.baidu.com" },
-    //         { num: 6, heading: "标题6", content: "内容6", keyword1: "这是一个关键词", source: "http://www.baidu.com" }
-    //     ],
-    //     userInput: "",
-    //     isShow: false,
-    // },
+
+    el: "#wrap",
 
     data() {
         return {
-            info: [],
+            info: [], //存放接收的论文信息
+            outputInfo: [],
+            hotWordArr: [], //存放接收的top10信息
             pageNum: 10,
             pageNow: 1,
-            listData: [
-                { num: 1, heading: "标题1", content: "内容1", keyword1: "这是一个关键词", source: "http://www.baidu.com" },
-                { num: 2, heading: "标题2", content: "内容2", keyword1: "这是一个关键词", source: "http://www.bilibili.com" },
-                { num: 3, heading: "标题3", content: "内容3", keyword1: "这是一个关键词", source: "http://www.baidu.com" },
-                { num: 4, heading: "标题4", content: "内容4", keyword1: "这是一个关键词", source: "http://www.baidu.com" },
-                { num: 5, heading: "标题5", content: "内容5", keyword1: "这是一个关键词", source: "http://www.baidu.com" },
-                { num: 6, heading: "标题6", content: "内容6", keyword1: "这是一个关键词", source: "http://www.baidu.com" }
-            ],
             userInput: "",
             isShow: false,
         }
+    },
+
+    mounted() {
+        //axios.get("http://localhost:8080/getKeyWordBySeg").then(response => (this.hotWordArr = response.data.data));
+        axios.get("./hotWord.json").then(response => (this.hotWordArr = response.data.data));
     },
 
     methods: {
 
         doSearching() {
             //搜索时默认从第一页获取
-            //向后端发送最新的userInput和page=1
+            //向后端发送最新的userInput
             this.pageNow = 1;
-            this.update();
+            axios.get("./search.json").then(response => {
+                this.info = response.data.data; //获取论文信息   
+                this.pageNum = Math.ceil(this.info.length / 6); //获取条数
+                this.outputInfo = this.info.slice(0, 6);
+
+            });
+
             this.isShow = true;
+        },
+
+        doSearchingwithHotWord(p) {
+            alert("查询：" + p);
         },
 
         pageUp: function() {
             if (this.pageNow > 1) {
                 this.pageNow--;
-                //向后端发送已有的userInput和当前page
-                this.update();
+                //
+                this.outputInfo = this.info.slice((this.pageNow - 1) * 6, (this.pageNow - 1) * 6 + 6); //(0,6)获取012345 (6,12)获取6 7 8 9 10 11
             } else {
                 //alert("已经是第一页！");
             }
@@ -71,37 +53,21 @@ var main = new Vue({
 
             if (this.pageNow < this.pageNum) {
                 this.pageNow++;
-                //向后端发送已有的userInput和当前page
-                this.update();
+                //
+                if (this.info.length >= this.pageNow * 6) { //有充足的项目渲染
+                    this.outputInfo = this.info.slice((this.pageNow - 1) * 6, (this.pageNow - 1) * 6 + 6); //(0,6)获取012345 (6,12)获取6 7 8 9 10 11
+                } else {
+                    this.outputInfo = this.info.slice((this.pageNow - 1) * 6, this.info.length);
+                }
+
             } else {
                 //alert("已经是最后一页！");
             }
         },
 
-        deleteArticle: function() {
-
+        deleteArticle: function(p) {
+            alert("正在删除" + p);
         },
-
-        update: function() {
-
-            for (var i = 0; i < 6; i++) {
-                this.listData[i].num = (this.pageNow - 1) * 6 + i + 1;
-                this.listData[i].heading = "标题" + ((this.pageNow - 1) * 6 + i + 1);
-                this.listData[i].content = "  搜索 (" + this.userInput + ") 的第" + this.pageNow + "分页";
-            }
-
-        },
-
-        t1: function() {
-            axios.get("./testdataL.json").then(response => (this.info = response.data.data));
-            console.log(this.info);
-        },
-    },
-
-
-
-
-
-
+    }
 
 })
