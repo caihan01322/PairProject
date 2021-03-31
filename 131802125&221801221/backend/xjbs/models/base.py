d670c87e96ecb11512447cd9a37f8d775a3fd94c
GONGHAIXU <41881187+GONGHAIXU@users.noreply.github.com>
2021-03-31 17:43:31 +0800

    Update base.py

--- 131802125&221801221/backend/xjbs/models/base.py
+++ 131802125&221801221/backend/xjbs/models/base.py
@@ -5,7 +5,7 @@ from werkzeug.middleware.dispatcher import DispatcherMiddleware
 app = Flask(__name__)
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://tempUser:tempPass123!@47.98.152.179:3306/xjbs'
+app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql://username:password!@ip:port/xjbs'
 
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
